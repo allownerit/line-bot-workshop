3e33a78bc7c96b4c92b03dabbc4edd3def2f2e56
allownerit <48045812+allownerit@users.noreply.github.com>
2019-02-27 14:17:27 +0700

    Update botpush.php

--- Desktop/line-bot-unlimit_friend-master/botpush.php
+++ Desktop/line-bot-unlimit_friend-master/botpush.php
@@ -8,7 +8,7 @@ $access_token = 'mzE/R/89otKiKDz4iF7lxmxpWZE5lUpI1gXlqocJcSmapxyowgdbuUu/ywcu4aa
 
 $channelSecret = '03a8707eba1fe6f3cfcefa112c432a95';
 // ID ที่ต้องการส่งไปหา
-$pushID = 'Uf38b64e24c3eab138b8ae9900370d29e';
+$pushID = 'U3a78c959bc4d4f415c3f17a6dbc7d585';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
