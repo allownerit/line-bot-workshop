210b0ca8f09609c4698a5118872300cec0a8fb5d
allownerit <48045812+allownerit@users.noreply.github.com>
2019-02-27 14:03:01 +0700

    Update verify.php

--- Desktop/line-bot-unlimit_friend-master/verify.php
+++ Desktop/line-bot-unlimit_friend-master/verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'Channel_Access_Token';
+$access_token = 'mzE/R/89otKiKDz4iF7lxmxpWZE5lUpI1gXlqocJcSmapxyowgdbuUu/ywcu4aaJMzWCRLUJ/mIjY4jf+eEXKjcZAQVMyJvb96VaMi0Z+K7grojBEa6+009rAmxsXCJmxXeG1c/C6TH9SQcoaHEQMwdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
